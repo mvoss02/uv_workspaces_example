02eaf74a4c9c3902d7756414a075ddf716973109
mvoss02 <m_l_voss@web.de>
2025-04-01 20:38:12 +0200

    Initial commit

---
+++ services/data_preprocessing/data_pre/main.py
@@ -0,0 +1,20 @@
+from my_package_1.lib import get_package_1
+from my_package_2.lib import get_package_2
+import numpy as np
+
+
+def data_pre_main():
+    print('Hello from data-preprocessing!')
+    
+    print('My Package 1:')
+    print(get_package_1())
+    
+    print('My Package 2:')
+    print(get_package_2())
+    
+    print('Generating a random matrix:')
+    print(np.random.rand(3,2))
+
+
+if __name__ == "__main__":
+    data_pre_main()
